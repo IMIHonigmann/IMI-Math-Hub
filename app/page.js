2f025805a262b967147debfd0c918221139f99e6
Homam Mousa <homammousa15@gmail.com>
2024-06-03 15:06:45 +0200

    added the matrix inversion prerequisite

--- app/page.js
+++ app/page.js
@@ -32,18 +32,18 @@ function Home() {
 
       <h3>Vector Projection</h3>
       <BlockMath math={formulas.vecProj} />
-      <GeoGebraTest />
+      {/* <GeoGebraTest /> */}
       
       <h3>Cross Product</h3>
       <BlockMath math={formulas.crossP} />
-      <GeoFicker />
+      {/* <GeoFicker /> */}
       <h3>Scalar Triple Product</h3>
       <BlockMath math={formulas.scalarTripleProduct} />
       <h3>Volume</h3>
       <BlockMath math={formulas.volume} />
 
       <div class='mx-5'>
-        <h1 class="text-5xl my-5"> Gaussian Elimination </h1>
+        <h1 class="text-5xl my-5" id="gaussianelimination"> Gaussian Elimination </h1>
         <p> Our Goal: Subtract the Rows with eachother (allowed in algebra btw) until you get a form that resembles stairs ascending to the left</p>
         <InlineMath math={`a_{21}, a_{31}, a_{32}`} /> <span> should all equal 0 in the end</span>
         <br/>
@@ -116,35 +116,86 @@ function Home() {
       </div>
 
       <h1 class="text-5xl my-5 mt-10"> Gauss-Jordan Elimination </h1>
-      <InlineMath math={formulas.gaussJordanS1} />
-      <br/>
-      <br/>
-      <InlineMath math={`II' = II - (I/b_{11} * 4)`} />
-      <br/>
-      <InlineMath math={`III' = III - (I/b_{11} * 4)`} />
-      <br/>
-      <InlineMath math={`IV' = IV - (I/b_{11} * 4)`} />
-      <br/>
-      <br/>
-      <InlineMath math={formulas.gaussJordanS2} />
-      <br/>
-      <br/>
-      <InlineMath math={formulas.gaussJordanS3} />
-      <br/>
-      <br/>
-      <InlineMath math={formulas.gaussJordanS4} />
-      <br/>
-      <br/>
-      <InlineMath math={formulas.gaussJordanS5} />
-      <br/>
-      <br/>
-      <InlineMath math={formulas.gaussJordanS6} />
-      <br/>
-      <br/>
-      <InlineMath math={formulas.gaussJordanFinal} />
-      {`<=>`}
-      <InlineMath math={formulas.unitMatrix} />
-    </div>
+        <p> Similar to <a href="#gaussianelimination">Gaussian Elimination</a> with the difference that we do the same operation again on the symmetric part </p>
+        <InlineMath math={formulas.gaussJordanFinal} />
+        <p> {`You might realize that this looks a lot like the unit matrix form and you'd be correct!`} </p>
+        <InlineMath math={formulas.gaussJordanFinal} />
+          {`<=>`}
+        <InlineMath math={formulas.unitMatrix} />
+        <br/>
+        <p> We can even augment it to the unit matrix by scaling every part if that is what we want </p>
+        <br/>
+        <InlineMath math={formulas.gaussJordanFinal} />
+        <br/>
+        <br/>
+        <InlineMath math={`I' = I/b_{11}`} />
+        <br/>
+        <InlineMath math={`II' = II/b_{22}`} />
+        <br/>
+        <InlineMath math={`III' = III/b_{33}`} />
+        <br/>
+        <InlineMath math={`IV' = IV/b_{44}`} />
+        <br/>
+        <br/>
+        <InlineMath math={formulas.unitMatrix} />
+        <br/>
+        <br/>
+        <p> {`Let's do it in action`} </p>
+        <div class='mx-5'>
+
+          
+          <InlineMath math={formulas.gaussJordanS1} />
+          <p class="mx-5">
+            <br/>
+            <InlineMath math={`II' = II - (I/b_{11} * 4)`} />
+            <br/>
+            <InlineMath math={`III' = III - (I/b_{11} * 4)`} />
+            <br/>
+            <InlineMath math={`IV' = IV - (I/b_{11} * 4)`} />
+          </p>
+          <br/>
+          <br/>
+          {'=>'}
+          <InlineMath math={formulas.gaussJordanS2} />
+          <br/>
+          <br/>
+          {'=>'}
+          <InlineMath math={formulas.gaussJordanS3} />
+          <br/>
+          <br/>
+          <p> {`Now repeat this step symmetrically`} </p>
+          {'=>'}
+          <InlineMath math={formulas.gaussJordanS4} />
+          <br/>
+          <br/>
+          {'=>'}
+          <InlineMath math={formulas.gaussJordanS5} />
+          <br/>
+          <br/>
+          {'=>'}
+          <InlineMath math={formulas.gaussJordanS6} />
+          <br/>
+          <br/>
+          {'=>'}
+          <InlineMath math={formulas.gaussJordanFinal} />
+          {`<=>`}
+          <InlineMath math={formulas.unitMatrix} />
+        <br/>
+        <br/>
+
+        <h1 class="text-5xl"> What is Linear Independence? </h1>
+        <div> {`Suppose we have 3 Vectors. If all of these vectors are linearly dependent then we can use the span of these vectors to represent any Vector in `}<InlineMath math={`\\mathbb{R}^3`} />. </div>
+        <div> {`Wanna see proof? Try it out. I guarantee you that you will be able to form any vector you want by just changing the coefficients of these 3 vectors`} </div>
+        <a href='./files/LinearIndependence.gbb' download='./files/LinearIndependence.gbb'> {`Try it out`} </a>
+        <br/>
+        <h1 class="text-5xl"> Matrix Inversion </h1>
+        <p> {`Before we start: I want you to remember the following`}</p>
+        <InlineMath math={`\\mathbf{A} * \\mathbf{B} \\neq \\mathbf{B} * \\mathbf{A}`} />
+        <br/>
+        <InlineMath math={`\\mathbf{A} * \\mathbf{A}^{-1} = \\mathbf{E} `} /> <span> {`<=>`} </span> <InlineMath math={` \\mathbf{A}^{-1} * \\mathbf{A} = \\mathbf{E}`} />
+      </div>
+        </div>
+
   );
 }
 
